ed2ccb3efc22d563cb50a115325e3456a61c3e9c
At0m1c7hund3r69 <thomasdachel72@gmail.com>
2025-12-03 18:56:36 -0500

    Done
    Win or Lose, This is it.

--- Assets/_3DStealthGame/Scripts/Observer.cs
+++ Assets/_3DStealthGame/Scripts/Observer.cs
@@ -16,14 +16,7 @@ public class Observer : MonoBehaviour
 		}
 	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            m_IsPlayerInRange = true;
-		
-        }
-    }
+
 
     void OnTriggerExit(Collider other)
     {
